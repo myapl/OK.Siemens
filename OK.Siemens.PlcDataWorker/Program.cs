e1cdea3bc004b8cfb594ead0cd630b9b5b3cae74
egoCheers <ego.cheers@gmail.com>
2022-02-17 15:37:23 +0300

    add serilog

--- OK.Siemens.PlcDataWorker/Program.cs
+++ OK.Siemens.PlcDataWorker/Program.cs
@@ -1,7 +1,25 @@
 using OK.Siemens.PlcDataWorker;
+using Serilog;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddJsonFile("appsettings.logs.json");
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .UseSerilog((hostContext, loggerConfiguration) =>
+    {
+        var appName = typeof(Program).Assembly.GetName().Name;
+        if (appName != null)
+            loggerConfiguration
+                .ReadFrom.Configuration(hostContext.Configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", appName)
+                .Enrich.WithProperty("Environment", hostContext.HostingEnvironment);
+    })
     .Build();
 
 await host.RunAsync();
