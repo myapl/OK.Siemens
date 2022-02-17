45b8501b0b3b619cfbc31b8d6d35aea5ae2f5c92
egoCheers <ego.cheers@gmail.com>
2022-02-17 15:29:09 +0300

    extract interface

--- OK.Siemens.DataProviders/PgsqlDataRecordsRepository.cs
+++ OK.Siemens.DataProviders/PgsqlDataRecordsRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace OK.Siemens.DataProviders;
+﻿using Microsoft.EntityFrameworkCore;
+using OK.Siemens.DataProviders.Interfaces;
+using OK.Siemens.Models;
 
-public class PgsqlDataRecordsRepository
+namespace OK.Siemens.DataProviders;
+
+public class PgsqlDataRecordsRepository : IDataRecordsRepository
 {
+    private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
     
+    public PgsqlDataRecordsRepository(IDbContextFactory<AppDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+    /// <summary>
+    /// Select data records between after and before
+    /// </summary>
+    /// <param name="before"></param>
+    /// <param name="after"></param>
+    /// <returns></returns>
+    public async Task<IQueryable<DataRecord>> GetRecordsBetweenTime(DateTime after, DateTime before)
+    {
+        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        return dbContext.DataRecords.Where(d => d.TimeStamp >= after && d.TimeStamp <= before).AsQueryable();
+    }
+
+    /// <summary>
+    /// Add collection of records to repository
+    /// </summary>
+    /// <param name="dataRecords"></param>
+    public async Task AddDataRecordsAsync(IEnumerable<DataRecord> dataRecords)
+    {
+        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        await dbContext.AddRangeAsync(dataRecords);
+        await dbContext.SaveChangesAsync();
+    }
 }
