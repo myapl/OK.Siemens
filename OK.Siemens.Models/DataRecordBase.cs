dec24d0d53d555ce91ed1f9ca7e7e1bad149ce7a
egoCheers <ego.cheers@gmail.com>
2022-02-17 21:44:35 +0300

    remove the id

--- OK.Siemens.Models/DataRecordBase.cs
+++ OK.Siemens.Models/DataRecordBase.cs
@@ -5,6 +5,5 @@ namespace OK.Siemens.Models;
 public abstract class DataRecordBase
 {
     public DateTime TimeStamp { get; set; }
-    [MaxLength(64)]
-    public string TagName { get; set; } = "";
+    [MaxLength(64)] public string TagName { get; set; } = "";
 }
