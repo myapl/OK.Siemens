4ee7528405e64420736a186f085f97b4b6ef1b7f
egoCheers <ego.cheers@gmail.com>
2022-02-17 15:27:16 +0300

    extracted interface for db repository

---
+++ OK.Siemens.DataProviders/Interfaces/IDataRecordsRepository.cs
@@ -0,0 +1,20 @@
+﻿using OK.Siemens.Models;
+
+namespace OK.Siemens.DataProviders.Interfaces;
+
+public interface IDataRecordsRepository
+{
+    /// <summary>
+    /// Select data records between after and before
+    /// </summary>
+    /// <param name="before"></param>
+    /// <param name="after"></param>
+    /// <returns></returns>
+    Task<IQueryable<DataRecord>> GetRecordsBetweenTime(DateTime after, DateTime before);
+
+    /// <summary>
+    /// Add collection of records to repository
+    /// </summary>
+    /// <param name="dataRecords"></param>
+    Task AddDataRecordsAsync(IEnumerable<DataRecord> dataRecords);
+}
