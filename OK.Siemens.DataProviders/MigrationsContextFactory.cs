120ef805a7b7bf8454899149c65397e26b750bbc
egoCheers <ego.cheers@gmail.com>
2022-02-17 21:44:57 +0300

    this needs for migrations in developing mode

---
+++ OK.Siemens.DataProviders/MigrationsContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OK.Siemens.DataProviders;
+
+/// <summary>
+/// For migrations in design time
+/// </summary>
+public class MigrationsContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5435;Database=siemens;Username=postgres;Password=dbpass");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
