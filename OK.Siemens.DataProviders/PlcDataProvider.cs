67cfd419f6d7e00365b770d7dcce218b9290f7c1
egoCheers <ego.cheers@gmail.com>
2022-02-17 22:03:52 +0300

    initial create

---
+++ OK.Siemens.DataProviders/PlcDataProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace OK.Siemens.DataProviders;
+
+public class PlcDataProvider
+{
+    private readonly Sharp7 _plc;
+
+    public PlcDataProvider()
+    {
+        _plc = new Sharp7();
+    }
+}
