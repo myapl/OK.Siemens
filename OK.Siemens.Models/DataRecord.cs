72266f16d1c9f89a661972670a3d806538f24ceb
egoCheers <ego.cheers@gmail.com>
2022-02-17 10:06:24 +0300

    add necessary fields to DataRecord model

---
+++ OK.Siemens.Models/DataRecord.cs
@@ -0,0 +1,6 @@
+﻿namespace OK.Siemens.Models;
+
+public class DataRecord: DataRecordBase
+{
+    public double Value { get; set; }
+}
--- OK.Siemens.Models/DataRecordBase.cs
+++ OK.Siemens.Models/DataRecordBase.cs
@@ -1,5 +1,10 @@
-﻿namespace OK.Siemens.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OK.Siemens.Models;
 
 public abstract class DataRecordBase
 {
+    public DateTime TimeStamp { get; set; }
+    [MaxLength(64)]
+    public string TagName { get; set; } = "";
 }
