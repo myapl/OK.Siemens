2d60026862fb6f920b078ef852ee3c8e66819682
egoCheers <ego.cheers@gmail.com>
2022-02-17 15:26:52 +0300

    application db context

---
+++ OK.Siemens.DataProviders/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OK.Siemens.Models;
+
+namespace OK.Siemens.DataProviders;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options)
+    { }
+
+    public virtual DbSet<DataRecord> DataRecords { get; set; } = null!;
+}
