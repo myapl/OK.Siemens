e216186e6362e772cd871ab509e612d25b29705e
egoCheers <ego.cheers@gmail.com>
2022-02-18 07:29:12 +0300

    plc connection settings class

---
+++ OK.Siemens.Models/PlcParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace OK.Siemens.Models;
+
+public class PlcParameters
+{
+    public const string Parameters = "PlcParameters";
+    public string Address { get; set; } = "";
+    public int Rack { get; set; }
+    public int Slot { get; set; }
+}
