153ade20715de2d5d8302638892abf0a5bffb8d9
egoCheers <ego.cheers@gmail.com>
2022-02-18 07:30:56 +0300

    read plc data

--- OK.Siemens.PlcDataWorker/Worker.cs
+++ OK.Siemens.PlcDataWorker/Worker.cs
@@ -1,20 +1,34 @@
+using OK.Siemens.DataProviders;
+using OK.Siemens.DataProviders.Interfaces;
+
 namespace OK.Siemens.PlcDataWorker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IPlcSiemensClient _client;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(IPlcSiemensClient client, ILogger<Worker> logger)
     {
         _logger = logger;
+        _client = client;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            if (!_client.IsConnected)
+            {
+                _client.Connect();           
+            }
+
+            if (_client.IsConnected)
+            {
+                _client.Read();
+            }
+            //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(100, stoppingToken);
         }
     }
 }
