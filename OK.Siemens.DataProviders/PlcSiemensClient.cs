761f040fec963ae61fbe533a75c33a203bbc8939
egoCheers <ego.cheers@gmail.com>
2022-02-18 07:29:44 +0300

    connect, read, dispose

--- OK.Siemens.DataProviders/PlcSiemensClient.cs
+++ OK.Siemens.DataProviders/PlcSiemensClient.cs
@@ -1,11 +1,51 @@
-﻿namespace OK.Siemens.DataProviders;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using OK.Siemens.DataProviders.Interfaces;
+using OK.Siemens.Models;
+using static OK.Siemens.DataProviders.Sharp7;
 
-public class PlcSiemensClient
+namespace OK.Siemens.DataProviders;
+
+public class PlcSiemensClient: IDisposable, IPlcSiemensClient
 {
-    private readonly Sharp7 _plc;
+    private readonly S7Client _client;
+    private readonly PlcParameters _plcParameters;
+    private readonly ILogger<PlcSiemensClient> _logger;
+    
+    public bool IsConnected { get => _client.Connected; }
 
-    public PlcSiemensClient()
+    public PlcSiemensClient(IOptions<PlcParameters> plcParameters, ILogger<PlcSiemensClient> logger)
     {
-        _plc = new Sharp7();
+        _client = new S7Client();
+        _plcParameters = plcParameters.Value;
+        _logger = logger;
+    }
+
+    public int Connect()
+    {
+        var error = _client.ConnectTo(_plcParameters.Address, _plcParameters.Rack, _plcParameters.Slot);
+        if (error == 0)
+            _logger.LogInformation("Connected to PLC [{address}]", _plcParameters.Address);
+        else
+            _logger.LogWarning("Error connecting to PLC [{address}], error [{error}]", _plcParameters.Address, error);
+        return error;
+    }
+
+    public int Read()
+    {
+        var data = new byte[1024];
+        var error = _client.ReadArea(S7Consts.S7AreaDB, 2, 0, 2, S7Consts.S7WLReal, data);
+        if (error == 0)
+            _logger.LogInformation("Success reading data from PLC [{address}]", _plcParameters.Address);
+        else
+            _logger.LogWarning("Error reading data from PLC [{address}], error [{error}]", _plcParameters.Address, error);
+        return error;
+    }
+    
+    public void Dispose()
+    {
+        _logger.LogInformation("PLC disconnecting...");
+        _client.Disconnect();
+        _logger.LogInformation("Disconnected");
     }
 }
