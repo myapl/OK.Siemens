aff043b0173eb91a35cfb628a1396aaf2d499a47
egoCheers <ego.cheers@gmail.com>
2022-02-19 18:33:55 +0300

    add tags, get tags, some improvements in add data records

--- OK.Siemens.DataProviders/PgsqlDataRecordsRepository.cs
+++ OK.Siemens.DataProviders/PgsqlDataRecordsRepository.cs
@@ -20,8 +20,8 @@ public class PgsqlDataRecordsRepository : IDataRecordsRepository
     /// <returns></returns>
     public async Task<IQueryable<DataRecord>> GetRecordsBetweenTime(DateTime after, DateTime before)
     {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
-        return dbContext.DataRecords.Where(d => d.TimeStamp >= after && d.TimeStamp <= before).AsQueryable();
+        var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        return dbContext.DataRecords.AsNoTracking().Where(d => d.TimeStamp >= after && d.TimeStamp <= before).AsQueryable();
     }
 
     /// <summary>
@@ -30,8 +30,41 @@ public class PgsqlDataRecordsRepository : IDataRecordsRepository
     /// <param name="dataRecords"></param>
     public async Task AddDataRecordsAsync(IEnumerable<DataRecord> dataRecords)
     {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
-        await dbContext.AddRangeAsync(dataRecords);
+        var dbContext = await _dbContextFactory.CreateDbContextAsync();
+
+        var enumerable = dataRecords.ToList();
+        foreach (var entry in enumerable)
+            dbContext.Entry(entry.TagName).State = EntityState.Unchanged;
+
+        await dbContext.DataRecords.AddRangeAsync(enumerable);
         await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Add plc tags collection to repository
+    /// </summary>
+    /// <param name="tags"></param>
+    public async Task AddTagsAsync(IEnumerable<PlcTag> tags)
+    {
+        var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        foreach (var tag in tags)
+        {
+            var tagPresent = await dbContext.Tags.FirstOrDefaultAsync(t => t.Tagname == tag.Tagname);
+            if (tagPresent == null)
+            {
+                await dbContext.AddAsync(tag);
+            }
+        }
+        await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Return all plc tags from repository
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IQueryable<PlcTag>> GetTagsAsync()
+    {
+        var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        return dbContext.Tags.AsNoTracking().AsQueryable();
     }
 }
