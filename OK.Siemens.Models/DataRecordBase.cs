421a38fd551d64920eba32418c5fcaad5f42af2b
egoCheers <ego.cheers@gmail.com>
2022-02-19 18:25:00 +0300

    Tagname become integer (key to tags table)

--- OK.Siemens.Models/DataRecordBase.cs
+++ OK.Siemens.Models/DataRecordBase.cs
@@ -6,5 +6,6 @@ public abstract class DataRecordBase
 {
     public long Id { get; set; }
     public DateTime TimeStamp { get; set; }
-    [MaxLength(64)] public string TagName { get; set; } = "";
+    public PlcTag TagName { get; set; } = new();
+    // [MaxLength(64)] public string TagName { get; set; } = "";
 }
