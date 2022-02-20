bb0f633617069d7ccfbf5f446905fbd4613a88f4
egoCheers <ego.cheers@gmail.com>
2022-02-20 14:26:47 +0300

    data context

--- OK.Siemens.TrendsWpf/MainWindow.xaml.cs
+++ OK.Siemens.TrendsWpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OK.Siemens.TrendsWpf.ViewModels;
 
 namespace OK.Siemens.TrendsWpf
 {
@@ -20,8 +21,9 @@ namespace OK.Siemens.TrendsWpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowViewModel viewModel)
         {
+            this.DataContext = viewModel;
             InitializeComponent();
         }
     }
