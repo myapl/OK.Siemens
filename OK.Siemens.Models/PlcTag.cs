6207b201fc4aefb0418ddf381fc6abad6c7e5da8
egoCheers <ego.cheers@gmail.com>
2022-02-20 14:31:37 +0300

    change explisit cast to the method called ToDataRecord

--- OK.Siemens.Models/PlcTag.cs
+++ OK.Siemens.Models/PlcTag.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore;
 
 namespace OK.Siemens.Models;
@@ -6,43 +7,44 @@ namespace OK.Siemens.Models;
 public class PlcTag
 {
     public int Id { get; set; }
-    public string Tagname { get; set; } = "";
+    public string TagName { get; set; } = "";
     public DataType DataType { get; set; }
     public DbAddress DbAddress { get; set; } = new();
     public string Description { get; set; } = "";
     [NotMapped] public object Data { get; set; } = "";
+
+    public Category? Category { get; set; }
     
     public List<DataRecord> DataRecords { get; set; } = new();
-    
-    
 
-    public static explicit operator DataRecord(PlcTag plcTag)
+    public DataRecord ToDataRecord(DateTime dateTime)
     {
+        if (dateTime.Kind != DateTimeKind.Utc)
+            throw new ArgumentOutOfRangeException(nameof(dateTime), "DateTime should be the Utc kind");
+        
         var item = new DataRecord
         {
-            TagName = plcTag,
-            TimeStamp = DateTime.UtcNow
+            TagName = this,
+            TimeStamp = dateTime
         };
 
-        plcTag.DataRecords.Add(item);
-
-        switch (plcTag.DataType)
+        switch (DataType)
         {
             case DataType.Bool:
-                item.Value = (bool) plcTag.Data ? 1.0f : 0.0f;
+                item.Value = (bool) Data ? 1.0f : 0.0f;
                 break;
             case DataType.Word:
                 throw new NotImplementedException();
             case DataType.DWord:
                 throw new NotImplementedException();
             case DataType.UInt:
-                item.Value = (ushort) plcTag.Data;
+                item.Value = (ushort) Data;
                 break;
             case DataType.Int:
-                item.Value = (int) plcTag.Data;
+                item.Value = (int) Data;
                 break;
             case DataType.Real:
-                item.Value = (float) plcTag.Data;
+                item.Value = (float) Data;
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
