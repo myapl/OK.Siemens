0da5d1b0045c1755e0939e62a4606a81466e9015
egoCheers <ego.cheers@gmail.com>
2022-02-20 14:32:18 +0300

    need to have one exact time for all received tags

--- OK.Siemens.PlcDataWorker/Worker.cs
+++ OK.Siemens.PlcDataWorker/Worker.cs
@@ -23,7 +23,6 @@ public class Worker : BackgroundService
         var dataRecords = new List<DataRecord>();
 
         await _repository.AddTagsAsync(_plcDbParser.ParseCsvFile("tags.csv"));
-
         var tagsList = await _repository.GetTagsAsync();
 
         while (!stoppingToken.IsCancellationRequested)
@@ -36,19 +35,18 @@ public class Worker : BackgroundService
             if (_client.IsConnected)
             {
                 var (error, tags) = _client.Read(20, tagsList.ToList());
-
                 if (error == 0)
                 {
                     dataRecords.Clear();
-
+                    var receivedTime = DateTime.UtcNow;
                     foreach (var tag in tags)
-                        dataRecords.Add((DataRecord) tag);
+                        dataRecords.Add(tag.ToDataRecord(receivedTime));
 
                     await _repository.AddDataRecordsAsync(dataRecords);
                 }
             }
             //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(300, stoppingToken);
+            // await Task.Delay(300, stoppingToken);
         }
     }
 }
