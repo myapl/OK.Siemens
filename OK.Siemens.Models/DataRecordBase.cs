7259a38997cb9bacba91bc53487f6ca109c4aaa1
egoCheers <ego.cheers@gmail.com>
2022-02-20 14:25:13 +0300

    delete commented line

--- OK.Siemens.Models/DataRecordBase.cs
+++ OK.Siemens.Models/DataRecordBase.cs
@@ -7,5 +7,4 @@ public abstract class DataRecordBase
     public long Id { get; set; }
     public DateTime TimeStamp { get; set; }
     public PlcTag TagName { get; set; } = new();
-    // [MaxLength(64)] public string TagName { get; set; } = "";
 }
