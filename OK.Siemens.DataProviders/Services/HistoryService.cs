2fa375008e16011a0c0ba43f766332a47739bd4b
egoCheers <ego.cheers@gmail.com>
2022-02-20 14:25:31 +0300

    adding a history service

---
+++ OK.Siemens.DataProviders/Services/HistoryService.cs
@@ -0,0 +1,24 @@
+﻿using OK.Siemens.DataProviders.Interfaces;
+using OK.Siemens.Models;
+
+namespace OK.Siemens.DataProviders.Services;
+
+public class HistoryService
+{
+    private readonly IDataRecordsRepository _repository;
+
+    public HistoryService(IDataRecordsRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    /// <summary>
+    /// Add tags category to repository
+    /// </summary>
+    /// <param name="categoryName"></param>
+    /// <returns></returns>
+    public async Task<(bool error, string message)> AddCategoryAsync(string categoryName)
+    {
+        return await _repository.AddCategoryAsync(new Category{Name = categoryName});
+    }
+}
