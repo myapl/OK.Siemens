30bf6a5484169f12b0bac994f91c5f44dcf127b2
egoCheers <ego.cheers@gmail.com>
2022-02-21 07:28:41 +0300

    initial create

---
+++ OK.Siemens.WPF.TrendClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace OK.Siemens.WPF.TrendClient.ViewModels;
+
+public class MainWindowViewModel: ObservableObject
+{
+    public ICommand OpenAddCategoryWindowCommand { get; }
+    public ICommand CancelAddCategoryCommand { get; }
+
+    private bool _isAddCategoryModalVisible;
+
+    public MainWindowViewModel()
+    {
+        OpenAddCategoryWindowCommand = new RelayCommand(OpenAddCategoryWindow);
+        CancelAddCategoryCommand = new RelayCommand(CancelAddCategory);
+    }
+
+    public bool IsAddCategoryModalVisible
+    {
+        get => _isAddCategoryModalVisible;
+        set => SetProperty(ref _isAddCategoryModalVisible, value);
+    }
+    
+    private void OpenAddCategoryWindow()
+    {
+        IsAddCategoryModalVisible = true;
+    }
+
+    private void CancelAddCategory()
+    {
+        IsAddCategoryModalVisible = false;
+    }
+}
