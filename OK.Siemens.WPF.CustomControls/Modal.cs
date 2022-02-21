d21f700125c40f7623c3677441cbd1e6ab8e792c
egoCheers <ego.cheers@gmail.com>
2022-02-21 07:29:52 +0300

    initial create

---
+++ OK.Siemens.WPF.CustomControls/Modal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OK.Siemens.WPF.CustomControls
+{
+    
+    public class Modal : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+            "IsOpen", typeof(bool), typeof(Modal), new PropertyMetadata(default(bool)));
+
+        public bool IsOpen
+        {
+            get { return (bool) GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+        
+        static Modal()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Modal),
+                new FrameworkPropertyMetadata(typeof(Modal)));
+        }
+    }
+}
---
+++ OK.Siemens.WPF.TrendClient/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OK.Siemens.DataProviders.Interfaces;
+using OK.Siemens.DataProviders.Services;
+using OK.Siemens.WPF.TrendClient.ViewModels;
+
+namespace OK.Siemens.WPF.TrendClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //private readonly IConfiguration _configuration;
+        
+        // public App()
+        // {
+            // var builder = new ConfigurationBuilder()
+            //     .SetBasePath(Directory.GetCurrentDirectory())
+            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            // _configuration = builder.Build();
+        // }
+
+        /// <summary>
+        /// startup event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void AppStartup(object sender, StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            
+            var serviceProvider = services.BuildServiceProvider();
+            
+            var mainWindowViewModel = serviceProvider.GetService<MainWindowViewModel>();
+            
+            
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddScoped<IHistoryService, HistoryService>()
+                .AddSingleton<MainWindowViewModel>()
+                .AddSingleton<MainWindow>();
+        }
+    }
+}
